5efc2aabac080e240cbe4d5310e207340416e025
louri45 <l.richard45@gmail.com>
2016-10-06 20:04:43 +0200

    Maj deezer api

--- deezer_api.php
+++ deezer_api.php
@@ -4,7 +4,9 @@
     $file_deezer = file_get_contents($query_deezer);
     $decode_deezer = json_decode($file_deezer, true);
 
+    var_dump($decode_deezer);
+
     echo 'le titre selectioné est : '.$decode_deezer['data'][0]['title'];
-    echo '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=false&width=300&height=300&color=076abf&layout=&size=medium&type=tracks&id='.$decode_deezer['data'][0]['id'].'&app_id=1" width="300" height="300"></iframe>';
+    echo '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=true&width=300&height=300&color=076abf&layout=&size=medium&type=tracks&id='.$decode_deezer['data'][0]['id'].'&app_id=1" width="300" height="300"></iframe>';
 
 
