bf751e68c499e39e22a8e6e8c1a734d8b50dc9fc
Jasdero <houssemaine.j@gmail.com>
2016-10-06 18:48:43 +0200

    changement_variable

--- weather_api.php
+++ weather_api.php
@@ -4,12 +4,12 @@ foreach ($_POST as $key => $value){
     $post[$key] = trim(htmlentities($value));
 }
 
-$titre = $post['search'];
+$ville = $post['search'];
 
 
-$query_weather = "http://api.openweathermap.org/data/2.5/weather?q='$titre'&APPID=aaf77036d9252af1eb6e40d782a49bf0";
+$query_weather = "http://api.openweathermap.org/data/2.5/weather?q='$ville'&APPID=aaf77036d9252af1eb6e40d782a49bf0";
 $file_weather = file_get_contents($$query_weather);
 $decode = json_decode($file_weather, true);
 
 
-echo $decode['weather'][0]['main'];
+$weather_type = $decode['weather'][0]['main'];
