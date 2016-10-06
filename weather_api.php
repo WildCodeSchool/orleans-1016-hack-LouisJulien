edbf4ef667d66ed390bf0c48460efe644c8800b2
Jasdero <houssemaine.j@gmail.com>
2016-10-06 18:45:35 +0200

    include weather api2

--- weather_api.php
+++ weather_api.php
@@ -7,9 +7,9 @@ foreach ($_POST as $key => $value){
 $titre = $post['search'];
 
 
-$query = "http://api.openweathermap.org/data/2.5/weather?q='$titre'&APPID=aaf77036d9252af1eb6e40d782a49bf0";
-$file = file_get_contents($query);
-$decode = json_decode($file, true);
+$query_weather = "http://api.openweathermap.org/data/2.5/weather?q='$titre'&APPID=aaf77036d9252af1eb6e40d782a49bf0";
+$file_weather = file_get_contents($$query_weather);
+$decode = json_decode($file_weather, true);
 
 
 echo $decode['weather'][0]['main'];
